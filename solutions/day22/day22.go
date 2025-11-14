a3c3aa7f7332f7f19daa1d26fcccada470cf2cbf
dubtar <dubtar@yandex.ru>
2025-11-14 17:59:58 +0300

    22

--- solutions/day22/day22.go
+++ solutions/day22/day22.go
@@ -3,207 +3,93 @@ package day22
 import (
 	"fmt"
 	h "go-aoc-template/internal/helpers"
+	"os"
 	"strings"
-
-	pq "github.com/idsulik/go-collections/v2/priorityqueue"
 )
 
-type EffectFunc func(*Game)
-
-type Effect struct {
-	turnsLeft int
-	apply     EffectFunc
-	stop      EffectFunc
-	name      string
+type Cell struct {
+	used      int
+	total     int
+	available int
+	coord     h.Coords
 }
 
-type Game struct {
-	personHitpoints int
-	personArmor     int
-	personMana      int
-	bossHitpoints   int
-	bossDamage      int
-	extraDamage     int
-	effects         []Effect
-	manaSpent       int
-	nextSpell       Spell
-	log             []string
-}
+func buildGrid(lines []string) (grid map[h.Coords]Cell, maxX int64, maxY int64) {
+	grid = make(map[h.Coords]Cell, len(lines))
+	maxX = -1
+	maxY = -1
 
-func (g *Game) ApplyEffects() {
-	next_effects := make([]Effect, 0, len(g.effects))
-	for _, effect := range g.effects {
-		if effect.apply != nil {
-			effect.apply(g)
-		}
-		effect.turnsLeft -= 1
-		g.log[len(g.log)-1] += fmt.Sprintf(". %d left", effect.turnsLeft)
-		if effect.turnsLeft <= 0 {
-			if effect.stop != nil {
-				effect.stop(g)
-			}
-		} else {
-			next_effects = append(next_effects, effect)
-		}
-	}
-	g.effects = next_effects
-}
-
-func castMissile(game *Game) {
-	game.bossHitpoints -= 4
-	game.log = append(game.log, fmt.Sprintf("Casts Missile, boss HP=%d", game.bossHitpoints))
-}
-func castDrain(game *Game) {
-	game.bossHitpoints -= 2
-	game.personHitpoints += 2
-	game.log = append(game.log, fmt.Sprintf("Casts Drain, boss HP=%d player HP=%d", game.bossHitpoints, game.personHitpoints))
-}
-func stopShield(game *Game) {
-	game.personArmor -= 7
-	game.log = append(game.log, fmt.Sprintf("Shield stops, player armor=%d", game.personArmor))
-}
-func castShield(game *Game) {
-	game.personArmor += 7
-	game.effects = append(game.effects, Effect{6, nil, stopShield, "shield"})
-	game.log = append(game.log, fmt.Sprintf("Shield casts, player armor=%d", game.personArmor))
-}
-
-func poison(game *Game) {
-	game.bossHitpoints -= 3
-	game.log = append(game.log, fmt.Sprintf("Poison effect, boss HP=%d", game.bossHitpoints))
-}
-func castPoison(game *Game) {
-	game.effects = append(game.effects, Effect{6, poison, nil, "poison"})
-	game.log = append(game.log, fmt.Sprintf("Poison cast"))
-}
-func recharge(game *Game) {
-	game.personMana += 101
-	game.log = append(game.log, fmt.Sprintf("Recharge effect, player mana=%d", game.personMana))
-}
-func castRecharge(game *Game) {
-	game.effects = append(game.effects, Effect{5, recharge, nil, "recharge"})
-	game.log = append(game.log, fmt.Sprintf("Recharge cast"))
-}
-
-type Spell struct {
-	name string
-	mana int
-	cast EffectFunc
-}
-
-var spells = []Spell{{"missile", 53, castMissile}, {"drain", 73, castDrain}, {"shield", 113, castShield}, {"poison", 173, castPoison}, {"recharge", 229, castRecharge}}
-
-func parseBoss(lines []string, game *Game) {
 	for _, line := range lines {
-		if strings.HasPrefix(line, "Hit Points:") {
-			game.bossHitpoints = int(h.ToInt(line[len("Hit Points: "):]))
-		} else if strings.HasPrefix(line, "Damage: ") {
-			game.bossDamage = int(h.ToInt(line[len("Damage: "):]))
-		}
-	}
-}
-
-func Run(init Game) int {
-	queue := pq.New(func(a, b Game) bool {
-		return a.manaSpent < b.manaSpent || a.manaSpent == b.manaSpent && a.nextSpell.mana < b.nextSpell.mana
-	})
-
-	for _, spell := range spells {
-		newgame := init
-		newgame.nextSpell = spell
-		queue.Push(newgame)
-	}
-	for {
-		game, ok := queue.Pop()
-		if !ok {
-			panic("No win found")
-		}
-		game.log = append(game.log,
-			fmt.Sprintf("--- Start turn: player HP=%d, mana=%d, armor=%d; Boss HP=%d",
-				game.personHitpoints,
-				game.personMana,
-				game.personArmor,
-				game.bossHitpoints))
-		if game.extraDamage > 0 {
-			game.personHitpoints -= game.extraDamage
-			game.log = append(game.log, fmt.Sprintf("Extra damage %d, HP=%d", game.extraDamage, game.personHitpoints))
-		}
-		if game.personHitpoints <= 0 {
-			continue // loose
-		}
-		game.ApplyEffects()
-		if game.bossHitpoints <= 0 {
-			// fmt.Println(strings.Join(game.log, "\n"))
-			return game.manaSpent // win
-		}
-		// person's turn
-		if game.personMana < game.nextSpell.mana {
-			continue // not enough mana
-		}
-		skip := false
-		for _, effect := range game.effects {
-			if effect.name == game.nextSpell.name {
-				skip = true
-				break
+		if strings.HasPrefix(line, "/dev/") {
+			cell := Cell{}
+			_, err := fmt.Sscanf(line, "/dev/grid/node-x%d-y%d %dT %dT %dT", &cell.coord.X,
+				&cell.coord.Y, &cell.total, &cell.used, &cell.available)
+			if err != nil {
+				panic(err)
 			}
-		}
-		if skip {
-			continue
-		}
-		game.nextSpell.cast(&game)
-		game.manaSpent += game.nextSpell.mana
-		game.personMana -= game.nextSpell.mana
-		if game.bossHitpoints <= 0 {
-			// fmt.Println(strings.Join(game.log, "\n"))
-			return game.manaSpent // win
-		}
-		// boss turn
-		game.log = append(game.log,
-			fmt.Sprintf("--- Boss turn: player HP=%d, mana=%d, armor=%d; Boss HP=%d; Cost=%d",
-				game.personHitpoints,
-				game.personMana,
-				game.personArmor,
-				game.bossHitpoints,
-				game.manaSpent))
-		game.ApplyEffects()
-		if game.bossHitpoints <= 0 {
-			// fmt.Println(strings.Join(game.log, "\n"))
-			return game.manaSpent // win
-		}
-		bossAttack := max(1, game.bossDamage-game.personArmor)
-		game.personHitpoints -= bossAttack
-		game.log = append(game.log, fmt.Sprintf("Boss attacks for %d damage, player HP=%d", bossAttack, game.personHitpoints))
-		if game.personHitpoints <= 0 {
-			continue // loose
-		}
-		for _, spell := range spells {
-			newgame := game
-			newgame.effects = nil
-			newgame.effects = append(newgame.effects, game.effects...)
-			newgame.log = nil
-			newgame.log = append(newgame.log, game.log...)
-			newgame.nextSpell = spell
-			queue.Push(newgame)
+			grid[cell.coord] = cell
+			maxX = max(maxX, cell.coord.X)
+			maxY = max(maxY, cell.coord.Y)
 		}
 	}
+	return grid, maxX, maxY
 }
 
 func PartOne(lines []string) string {
-	game := Game{personHitpoints: 50, personMana: 500}
-	parseBoss(lines, &game)
-	result := Run(game)
-	return fmt.Sprint(result)
+	grid, _, _ := buildGrid(lines)
+
+	result := 0
+	for _, cell := range grid {
+		if cell.used == 0 { // 1. cell A is not empty
+			continue
+		}
+		for _, other := range grid {
+			if cell.coord == other.coord { // 2. Nodes A and B are not the same node.
+				continue
+			}
+			if cell.used <= other.available { // 3. The data on node A (its Used) would fit on node B (its Avail).
+				result++
+			}
+		}
+	}
+	return fmt.Sprintf("%d", result)
 }
 
-func TestGame() Game {
-	return Game{
-		personHitpoints: 10, personMana: 250,
-		bossHitpoints: 13, bossDamage: 8,
-	}
-}
 func PartTwo(lines []string) string {
-	game := Game{personHitpoints: 50, personMana: 500, extraDamage: 1}
-	parseBoss(lines, &game)
-	result := Run(game)
-	return fmt.Sprint(result)
+	grid, maxX, maxY := buildGrid(lines)
+
+	// finish := h.Coords{X: 0, Y: 0}
+	// target := h.Coords{X: maxX, Y: 0}
+	zero := h.Coords{X: -1, Y: -1}
+	// save to /tmp/grid.txt file
+	file, err := os.Create("/tmp/grid.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	for y := range maxY {
+		for x := range maxX {
+			fmt.Fprintf(file, "%3d/%3d  ", grid[h.Coords{X: x, Y: y}].used, grid[h.Coords{X: x, Y: y}].total)
+		}
+		fmt.Fprintln(file)
+	}
+	for c, cell := range grid {
+		if cell.used == 0 {
+			zero = c
+			// break
+		}
+	}
+	if zero.X < 0 {
+		panic("zero cell not found")
+	}
+
+	result := zero.Y         // столько раз двигаем до первого ряда (упрощение, что 0 < zero.X < target.X)
+	result += maxX - zero.X  // столько раз двигаем до целевого столбца ( теперь target.X = maxX - 1)
+	result += (maxX - 1) * 5 // чтобы перейдвинуть цель на 1 влево
+	if maxX > 5 {
+		result += 12 // магическая добавка, глазами изучив данные увидел стенку, на обход которой нужно 6 в сторону шагов и 6 назад
+	}
+
+	return fmt.Sprintf("%d", result)
 }
--- solutions/day22/test22/test22.go
+++ solutions/day22/test22/test22.go
@@ -7,11 +7,21 @@ import (
 	day22 "go-aoc-template/solutions/day22"
 )
 
-var lines = strings.Split(`example input`, "\n")
+var lines = strings.Split(`Filesystem            Size  Used  Avail  Use%
+/dev/grid/node-x0-y0   10T    8T     2T   80%
+/dev/grid/node-x0-y1   11T    6T     5T   54%
+/dev/grid/node-x0-y2   32T   28T     4T   87%
+/dev/grid/node-x1-y0    9T    7T     2T   77%
+/dev/grid/node-x1-y1    8T    0T     8T    0%
+/dev/grid/node-x1-y2   11T    7T     4T   63%
+/dev/grid/node-x2-y0   10T    6T     4T   60%
+/dev/grid/node-x2-y1    9T    8T     1T   88%
+/dev/grid/node-x2-y2    9T    6T     3T   66%
+`, "\n")
 
 var (
-	partOneAnswer = ""
-	partTwoAnswer = "example answer"
+	partOneAnswer = "7"
+	partTwoAnswer = "7"
 )
 
 type SolutionFunc func([]string) string
@@ -27,7 +37,6 @@ func runTest(part int, solution SolutionFunc, expected string) {
 }
 
 func main() {
-	fmt.Printf("%v", day22.Run(day22.TestGame()))
-	// runTest(1, day22.PartOne, partOneAnswer)
-	// runTest(2, day22.PartTwo, partTwoAnswer)
+	runTest(1, day22.PartOne, partOneAnswer)
+	runTest(2, day22.PartTwo, partTwoAnswer)
 }
